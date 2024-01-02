980208e50370933f6b895430ff6784511c10b8e6
Maksim Shiryaev <shimady563@gmail.com>
2024-01-02 20:51:20 +0300

    feat(thymeleaf): add dependency and config classes, add template engine and webcontext to base servlet

--- src/main/java/project/coffeeshop/commons/CoffeeShopServlet.java
+++ src/main/java/project/coffeeshop/commons/CoffeeShopServlet.java
@@ -1,11 +1,17 @@
 package project.coffeeshop.commons;
 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.Thymeleaf;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 import project.coffeeshop.authentication.Session;
 
 import java.io.IOException;
@@ -13,10 +19,22 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Optional;
 
+import static project.coffeeshop.commons.ThymeleafConfig.getWebContext;
+
 public abstract class CoffeeShopServlet extends HttpServlet {
+    protected ITemplateEngine templateEngine;
+    protected WebContext webContext;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        templateEngine = (ITemplateEngine) config.getServletContext().getAttribute("templateEngine");
+        super.init(config);
+    }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        webContext = getWebContext(request, response, getServletContext());
+
         try {
             super.service(request, response);
         } catch (ServletException e) {
