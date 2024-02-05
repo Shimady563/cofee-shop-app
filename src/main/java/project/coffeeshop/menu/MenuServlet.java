060990e6b0be81131f27457e879a6face861db3e
Maksim Shiryaev <shimady563@gmail.com>
2024-02-05 21:24:06 +0300

    feat(menu): implement adding to favorites

--- src/main/java/project/coffeeshop/menu/MenuServlet.java
+++ src/main/java/project/coffeeshop/menu/MenuServlet.java
@@ -3,20 +3,33 @@ package project.coffeeshop.menu;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import project.coffeeshop.authentication.Session;
+import project.coffeeshop.authentication.SessionDao;
+import project.coffeeshop.authentication.User;
+import project.coffeeshop.authentication.UserDao;
 import project.coffeeshop.commons.CoffeeShopServlet;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static project.coffeeshop.commons.ServletUtil.findCookieByName;
 
 @WebServlet(name = "MenuServlet", value = "/menu")
 public class MenuServlet extends CoffeeShopServlet {
     private MenuDao menuDao;
+    private SessionDao sessionDao;
+    private UserDao userDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         menuDao = new MenuDao();
+        sessionDao = new SessionDao();
+        userDao = new UserDao();
         super.init(config);
     }
 
@@ -29,5 +42,27 @@ public class MenuServlet extends CoffeeShopServlet {
 
         webContext.setVariable("menuItems", menuItems);
         templateEngine.process("menu", webContext, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int menuItemId = Integer.parseInt(request.getParameter("menuItemId"));
+        Cookie[] cookies = request.getCookies();
+        Optional<Cookie> cookieOptional = findCookieByName(cookies, "sessionId");
+        if (cookieOptional.isPresent()) {
+            Optional<Session> sessionOptional = sessionDao.findById(UUID.fromString(cookieOptional.get().getValue()));
+
+            if (sessionOptional.isPresent()) {
+                Optional<User> userOptional = userDao.findById(sessionOptional.get().getUserId());
+
+                if (userOptional.isPresent()) {
+                    menuDao.saveToFavorites(userOptional.get().getId(), menuItemId);
+                    webContext.setVariable("itemId", menuItemId);
+                    webContext.setVariable("message", "Added to favorites");
+                }
+            }
+        }
+
+        doGet(request, response);
     }
 }
