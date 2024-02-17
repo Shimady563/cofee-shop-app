be5d487f569631dd5651aff902f89e417f604b8e
Maksim Shiryaev <shimady563@gmail.com>
2024-02-17 15:54:14 +0300

    feat(news): news are now shown properly add images for news examples add message when no news add call for newsDao

--- src/main/java/project/coffeeshop/news/NewsServlet.java
+++ src/main/java/project/coffeeshop/news/NewsServlet.java
@@ -1,5 +1,6 @@
 package project.coffeeshop.news;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,11 +8,23 @@ import jakarta.servlet.http.HttpServletResponse;
 import project.coffeeshop.commons.CoffeeShopServlet;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "NewsServlet", value = "/news")
 public class NewsServlet extends CoffeeShopServlet {
+    private NewsDao newsDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        newsDao = new NewsDao();
+        super.init(config);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<PieceOfNews> news = newsDao.findAll();
+
+        webContext.setVariable("news", news);
         templateEngine.process("news", webContext, response.getWriter());
     }
 }
