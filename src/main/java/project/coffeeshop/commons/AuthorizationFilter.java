d5dd854f6b1a3333230c4e00c465c05ab35688cb
Maksim Shiryaev <shimady563@gmail.com>
2024-03-03 16:42:43 +0300

    feat(cart): add cart servlet to filtration

--- src/main/java/project/coffeeshop/commons/AuthorizationFilter.java
+++ src/main/java/project/coffeeshop/commons/AuthorizationFilter.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 import static project.coffeeshop.commons.ServletUtil.*;
 
-@WebFilter(filterName = "AuthorizationFilter", servletNames = {"ProfileServlet", "MenuServlet", "FavoritesServlet", "NewsServlet", "PieceOfNewsServlet"})
+@WebFilter(filterName = "AuthorizationFilter", servletNames = {"ProfileServlet", "MenuServlet", "FavoritesServlet", "NewsServlet", "PieceOfNewsServlet", "CartServlet"})
 public class AuthorizationFilter extends HttpFilter {
     private SessionDao sessionDao;
 
