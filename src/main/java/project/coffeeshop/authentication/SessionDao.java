de6f34462b19e08736b5cb1def1074eef2a7e6b5
Maksim Shiryaev <shimady563@gmail.com>
2024-03-17 21:17:37 +0300

    feat(hibernate): change dao classes in authentication

--- src/main/java/project/coffeeshop/authentication/SessionDao.java
+++ src/main/java/project/coffeeshop/authentication/SessionDao.java
@@ -1,92 +1,103 @@
 package project.coffeeshop.authentication;
 
-import jakarta.servlet.ServletException;
+import project.coffeeshop.commons.AbstractDao;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-public class SessionDao {
-    private final DataSource dataSource;
+public class SessionDao extends AbstractDao<Session, UUID> {
 
-    public SessionDao() throws ServletException {
-        try {
-            Context context = new InitialContext();
-            Context envContext = (Context) context.lookup("java:comp/env");
-            this.dataSource = (DataSource) envContext.lookup("jdbc/db");
-        } catch (NamingException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public void save(Session session) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection
-                .prepareStatement("insert into public.session (id, user_id, expiration_time) values (?, ?, ?)")) {
-
-            preparedStatement.setObject(1, session.getId());
-            preparedStatement.setLong(2, session.getUserId());
-            preparedStatement.setTimestamp(3, Timestamp.valueOf(session.getExpirationTime()));
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public Optional<Session> findById(UUID sessionId) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection
-                .prepareStatement("select id, user_id, expiration_time from public.session where id = ?")) {
-
-            preparedStatement.setObject(1, sessionId);
-
-            preparedStatement.executeQuery();
-            ResultSet resultSet = preparedStatement.getResultSet();
-
-            Session session = null;
-            while (resultSet.next()) {
-                UUID id = (UUID) resultSet.getObject(1);
-                long userId = resultSet.getLong(2);
-                LocalDateTime expirationTime = resultSet.getTimestamp(3).toLocalDateTime();
-                session = new Session(id, userId, expirationTime);
-            }
-
-            return Optional.ofNullable(session);
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public void delete(UUID sessionId) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("delete from public.session where id = ?")) {
-
-            preparedStatement.setObject(1, sessionId);
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
+    public Optional<Session> findById(UUID id) {
+        return Optional.ofNullable(entityManager.find(Session.class, id));
     }
 
     public void deleteExpiredSessions(LocalDateTime now) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("delete from public.session where expiration_time < ?")) {
-
-            preparedStatement.setTimestamp(1, Timestamp.valueOf(now));
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-        }
+        entityManager
+                .createQuery("delete from Session " +
+                        "where expirationTime < :time")
+                .setParameter("time", now)
+                .executeUpdate();
     }
 }
+
+
+//public class SessionDao {
+//    private final DataSource dataSource;
+//
+//    public SessionDao() throws ServletException {
+//        try {
+//            Context context = new InitialContext();
+//            Context envContext = (Context) context.lookup("java:comp/env");
+//            this.dataSource = (DataSource) envContext.lookup("jdbc/db");
+//        } catch (NamingException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public void save(Session session) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//        PreparedStatement preparedStatement = connection
+//                .prepareStatement("insert into public.session (id, user_id, expiration_time) values (?, ?, ?)")) {
+//
+//            preparedStatement.setObject(1, session.getId());
+//            preparedStatement.setLong(2, session.getUserId());
+//            preparedStatement.setTimestamp(3, Timestamp.valueOf(session.getExpirationTime()));
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public Optional<Session> findById(UUID sessionId) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//        PreparedStatement preparedStatement = connection
+//                .prepareStatement("select id, user_id, expiration_time from public.session where id = ?")) {
+//
+//            preparedStatement.setObject(1, sessionId);
+//
+//            preparedStatement.executeQuery();
+//            ResultSet resultSet = preparedStatement.getResultSet();
+//
+//            Session session = null;
+//            while (resultSet.next()) {
+//                UUID id = (UUID) resultSet.getObject(1);
+//                long userId = resultSet.getLong(2);
+//                LocalDateTime expirationTime = resultSet.getTimestamp(3).toLocalDateTime();
+//                session = new Session(id, userId, expirationTime);
+//            }
+//
+//            return Optional.ofNullable(session);
+//
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public void delete(UUID sessionId) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("delete from public.session where id = ?")) {
+//
+//            preparedStatement.setObject(1, sessionId);
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public void deleteExpiredSessions(LocalDateTime now) {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("delete from public.session where expiration_time < ?")) {
+//
+//            preparedStatement.setTimestamp(1, Timestamp.valueOf(now));
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            System.err.println(e.getMessage());
+//        }
+//    }
+//}
--- src/main/java/project/coffeeshop/authentication/UserDao.java
+++ src/main/java/project/coffeeshop/authentication/UserDao.java
@@ -1,109 +1,127 @@
 package project.coffeeshop.authentication;
 
-import jakarta.servlet.ServletException;
+import jakarta.persistence.NoResultException;
+import project.coffeeshop.commons.AbstractDao;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Optional;
 
-public class UserDao {
-    private final DataSource dataSource;
+public class UserDao extends AbstractDao<User, Long> {
 
-    public UserDao() throws ServletException {
+    @Override
+    public Optional<User> findById(Long userId) {
+        return Optional.ofNullable(entityManager.find(User.class, userId));
+    }
+
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
         try {
-            Context context = new InitialContext();
-            Context envContext = (Context) context.lookup("java:comp/env");
-            this.dataSource = (DataSource) envContext.lookup("jdbc/db");
-        } catch (NamingException e) {
-            throw new ServletException(e);
+            return Optional
+                    .of(entityManager
+                            .createQuery("select User from User " +
+                                    "where username = :username", User.class)
+                            .setParameter("username", username)
+                            .getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
         }
-    }
-
-    public long save(User user) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("insert into public.\"user\" (username, password) values (?, ?)",
-                             PreparedStatement.RETURN_GENERATED_KEYS)) {
-
-            preparedStatement.setString(1, user.getUsername());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.executeUpdate();
-
-            long generatedId = -1;
-            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-            if (generatedKeys.next()) {
-               generatedId = generatedKeys.getLong(1);
-            }
-
-            return generatedId;
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public Optional<User> findByUsername(String username) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("select * from public.\"user\" where username = ?")) {
-
-            preparedStatement.setString(1, username);
-            preparedStatement.executeQuery();
-
-            return getUser(preparedStatement.getResultSet());
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public Optional<User> findById(Long id) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("select * from public.\"user\" where id = ?")) {
-
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeQuery();
-
-            return getUser(preparedStatement.getResultSet());
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    public void update(User user) throws ServletException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection
-                     .prepareStatement("update public.\"user\" " +
-                             "set username = ?, password = ? " +
-                             "where id = ?")) {
-
-            preparedStatement.setString(1, user.getUsername());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setLong(3, user.getId());
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
-    }
-
-    private Optional<User> getUser(ResultSet resultSet) throws SQLException {
-        User user = null;
-        while (resultSet.next()) {
-            long userId = resultSet.getLong(1);
-            String username = resultSet.getString(2);
-            String password = resultSet.getString(3);
-            user = new User(userId, username, password);
-        }
-
-        resultSet.close();
-
-        return Optional.ofNullable(user);
     }
 }
+
+//public class UserDao {
+//    private final DataSource dataSource;
+//
+//    public UserDao() throws ServletException {
+//        try {
+//            Context context = new InitialContext();
+//            Context envContext = (Context) context.lookup("java:comp/env");
+//            this.dataSource = (DataSource) envContext.lookup("jdbc/db");
+//        } catch (NamingException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public long save(User user) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("insert into public.\"user\" (username, password) values (?, ?)",
+//                             PreparedStatement.RETURN_GENERATED_KEYS)) {
+//
+//            preparedStatement.setString(1, user.getUsername());
+//            preparedStatement.setString(2, user.getPassword());
+//            preparedStatement.executeUpdate();
+//
+//            long generatedId = -1;
+//            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+//            if (generatedKeys.next()) {
+//               generatedId = generatedKeys.getLong(1);
+//            }
+//
+//            return generatedId;
+//
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public Optional<User> findByUsername(String username) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("select * from public.\"user\" where username = ?")) {
+//
+//            preparedStatement.setString(1, username);
+//            preparedStatement.executeQuery();
+//
+//            return getUser(preparedStatement.getResultSet());
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public Optional<User> findById(Long id) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("select * from public.\"user\" where id = ?")) {
+//
+//            preparedStatement.setLong(1, id);
+//            preparedStatement.executeQuery();
+//
+//            return getUser(preparedStatement.getResultSet());
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    public void update(User user) throws ServletException {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection
+//                     .prepareStatement("update public.\"user\" " +
+//                             "set username = ?, password = ? " +
+//                             "where id = ?")) {
+//
+//            preparedStatement.setString(1, user.getUsername());
+//            preparedStatement.setString(2, user.getPassword());
+//            preparedStatement.setLong(3, user.getId());
+//            preparedStatement.executeUpdate();
+//
+//        } catch (SQLException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//
+//    private Optional<User> getUser(ResultSet resultSet) throws SQLException {
+//        User user = null;
+//        while (resultSet.next()) {
+//            long userId = resultSet.getLong(1);
+//            String username = resultSet.getString(2);
+//            String password = resultSet.getString(3);
+//            user = new User(userId, username, password);
+//        }
+//
+//        resultSet.close();
+//
+//        return Optional.ofNullable(user);
+//    }
+//}
