5ce4c23d527f7026d47b898680bf6b3e36c72acf
Maksim Shiryaev <shimady563@gmail.com>
2024-03-17 21:13:55 +0300

    feat(hibernate): create hibernate util class

---
+++ src/main/java/project/coffeeshop/commons/HibernateUtil.java
@@ -0,0 +1,17 @@
+package project.coffeeshop.commons;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PersistenceUtil {
+    private static final EntityManagerFactory entityManagerFactory;
+
+    static {
+        entityManagerFactory = Persistence.createEntityManagerFactory("coffeeshopPersistenceUnit");
+    }
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+}
