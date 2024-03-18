e2f219ec91212a9c1f5d632d4979521915749c25
Shimady563 <shimady563@gmail.com>
2024-03-18 19:58:01 +0300

    test(hibernate): create session and user dao tests

---
+++ src/test/java/project/coffeeshop/authentication/UserDaoTest.java
@@ -0,0 +1,97 @@
+package project.coffeeshop.authentication;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDaoTest {
+    private static EntityManager entityManager;
+    private final UserDao userDao = new UserDao();
+
+    @BeforeAll
+    public static void setUpEntityManager() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("coffeeshopTestPersistenceUnit");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @BeforeEach
+    public void setUpUserDao() throws NoSuchFieldException, IllegalAccessException {
+        Field entityManagerField = userDao.getClass().getSuperclass().getDeclaredField("entityManager");
+        entityManagerField.setAccessible(true);
+        entityManagerField.set(userDao, entityManager);
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User("username", "pass");
+        userDao.save(user);
+
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    public void testFindById() {
+        User user = new User("username1", "pass1");
+        userDao.save(user);
+        Long userId = user.getId();
+        Optional<User> userOptional = userDao.findById(userId);
+
+        assertTrue(userOptional.isPresent());
+        assertEquals(userOptional.get().getId(), userId);
+
+        Long nonExistingId = 1000L;
+
+        assertTrue(userDao.findById(nonExistingId).isEmpty());
+    }
+
+    @Test
+    public void testFindByUsername() {
+        String username = "username2";
+        User user = new User(username, "pass1");
+        userDao.save(user);
+        Optional<User> userOptional = userDao.findByUsername(username);
+
+        assertTrue(userOptional.isPresent());
+        assertEquals(userOptional.get().getUsername(), username);
+
+        String nonExistingUsername = "notAUsername";
+
+        assertTrue(userDao.findByUsername(nonExistingUsername).isEmpty());
+    }
+
+    @Test
+    public void testDelete() {
+        User user = new User("username3", "pass3");
+        userDao.save(user);
+        Long userId = user.getId();
+
+        userDao.delete(user);
+
+        assertTrue(userDao.findById(userId).isEmpty());
+    }
+
+    @Test
+    public void testUpdate() throws ServletException {
+        User user = new User("username3", "pass3");
+        userDao.save(user);
+        Long userId = user.getId();
+        String newUsername = "newUsername";
+        String newPass = "newPass";
+        user.setUsername(newUsername);
+        user.setPassword(newPass);
+
+        userDao.update(user);
+
+        Optional<User> userOptional = userDao.findById(userId);
+        assertEquals(userOptional.get().getUsername(), newUsername);
+        assertEquals(userOptional.get().getPassword(), newPass);
+    }
+}
