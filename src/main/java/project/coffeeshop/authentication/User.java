150d1ab5b5777dc14138cd064cb9cb30c75d48d3
Maksim Shiryaev <shimady563@gmail.com>
2024-03-19 21:57:57 +0300

    feat(hibernate): initialize menu item entity create menu item entity and add many to many column to user entity

--- src/main/java/project/coffeeshop/authentication/User.java
+++ src/main/java/project/coffeeshop/authentication/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import project.coffeeshop.menu.MenuItem;
+
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -24,6 +27,12 @@ public class User {
     @Setter
     @Column(name = "password", nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_menu_item",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "menu_item_id"))
+    private Set<MenuItem> menuItems;
 
     public User(String username, String password) {
         this.username = username;
--- src/main/java/project/coffeeshop/menu/MenuItem.java
+++ src/main/java/project/coffeeshop/menu/MenuItem.java
@@ -1,16 +1,49 @@
 package project.coffeeshop.menu;
 
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.coffeeshop.authentication.User;
 
-@AllArgsConstructor
+import java.util.Set;
+
 @NoArgsConstructor
 @Getter
-@Setter
-@ToString
+@Entity
+@Table(name = "menu_item")
 public class MenuItem {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "price", nullable = false)
     private double price;
+
+    @Column(name = "volume", nullable = false)
     private int volume;
+
+    @Column(name = "image", nullable = false)
     private String image;
+
+    @ManyToMany(mappedBy = "menuItems", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_menu_item",
+            joinColumns = @JoinColumn(name = "menu_item_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users;
 }
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//public class MenuItem {
+//    private long id;
+//    private String name;
+//    private double price;
+//    private int volume;
+//    private String image;
+//}
