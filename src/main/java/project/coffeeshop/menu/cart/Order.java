f93fcca7af0d3894d9e0de8ef6f848660de0bad9
Maksim Shiryaev <shimady563@gmail.com>
2024-03-23 18:46:30 +0300

    syle: remove comments

--- src/main/java/project/coffeeshop/menu/cart/Order.java
+++ src/main/java/project/coffeeshop/menu/cart/Order.java
@@ -36,21 +36,3 @@ public class Order {
         this.user = user;
     }
 }
-
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Setter
-//@Getter
-//@ToString
-//public class Order {
-//    private long id;
-//    private LocalDateTime creationTime;
-//    private LocalDateTime readyTime;
-//    private double price;
-//
-//    public Order(LocalDateTime creationTime, LocalDateTime readyTime, double price) {
-//        this.creationTime = creationTime;
-//        this.readyTime = readyTime;
-//        this.price = price;
-//    }
-//}
